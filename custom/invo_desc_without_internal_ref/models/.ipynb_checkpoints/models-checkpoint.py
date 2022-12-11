57de1536b49966f623afd788575e174d935f20a6
sardo15 <andree.15.sardo@gmail.com>
2022-12-11 19:11:52 +0000

    corrigiendo

--- custom/invo_desc_without_internal_ref/models/.ipynb_checkpoints/models-checkpoint.py
+++ custom/invo_desc_without_internal_ref/models/.ipynb_checkpoints/models-checkpoint.py
@@ -1,7 +1,33 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
+    
+    _inherit = 'account.move.line'
+    
+    def _get_computed_name(self):
+        self.ensure_one()
 
+        if not self.product_id:
+            return ''
+
+        if self.partner_id.lang:
+            product = self.product_id.with_context(lang=self.partner_id.lang)
+        else:
+            product = self.product_id
+
+        values = []
+        if product.partner_ref:
+            values.append(product.partner_ref)
+        if self.journal_id.type == 'sale':
+            if product.description_sale:
+                values.append(product.description_sale)
+        elif self.journal_id.type == 'purchase':
+            if product.description_purchase:
+                values.append(product.description_purchase)
+        
+        val = '\n'.join(values)
+        ref = "["+product.default_code+"] "
+        return val.replace(ref,"",1)
 
 # class invo_desc_without_internal_ref(models.Model):
 #     _name = 'invo_desc_without_internal_ref.invo_desc_without_internal_ref'
